3db62190b379f5569a3022e1b1724a2fe3a1d94f
Chuiyi Lin <chuiyilin@gmail.com>
2015-07-03 19:25:01 +0800

    crawler start to work.

--- crawler.js
+++ crawler.js
@@ -1,5 +1,43 @@
-var crawler = require('crawler');
+var Crawler = require('crawler');
+var url = require('url');
 
-function crawlThis (url) {
-    // crawler.
-};
+var c = new Crawler({
+    maxConnections : 10,
+    // This will be called for each crawled page
+    callback : function (error, result, $) {
+        // $ is Cheerio by default
+        //a lean implementation of core jQuery designed specifically for the server
+        $('a').each(function(index, a) {
+            var toQueueUrl = $(a).attr('href');
+            if (toQueueUrl.indexOf('http') === 0 && toQueueUrl.indexOf('dmm.co.jp') > -1) {
+                console.log('crawl url: ' + toQueueUrl);
+                crawler_dmm_queue(toQueueUrl);
+            }
+        });
+    }
+});
+
+function crawler_dmm() {
+    c.queue('http://www.dmm.co.jp');
+}
+
+function crawler_dmm_queue(url) {
+    c.queue([{
+        uri: url,
+        jQuery: true,
+
+        callback: function (error, result, $) {
+            if($) {
+                $('a').each(function(index, a) {
+                    var toQueueUrl = $(a).attr('href');
+                    if (toQueueUrl.indexOf('http') === 0 && toQueueUrl.indexOf('dmm.co.jp') > -1) {
+                        console.log('crawl url: ' + toQueueUrl);
+                        crawler_dmm_queue(toQueueUrl);
+                    }
+                });
+            }
+        }
+    }]);
+}
+
+crawler_dmm();
