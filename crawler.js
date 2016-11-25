951388f17d8e39bf63f684719dcaa9af4a85ae32
Chuiyi Lin <chuiyilin@gmail.com>
2016-11-25 17:52:17 +0800

    Ver 0.0.1 11/25 daily commit

--- crawler.js
+++ crawler.js
@@ -1,6 +1,9 @@
-var Crawler = require('crawler');
 var url = require('url');
+var Crawler = require('crawler');
+var mongoose = require('mongoose');
+
 var url_array = [];
+var url_blacklist = [];
 
 var c = new Crawler({
     maxConnections : 10,
@@ -8,20 +11,9 @@ var c = new Crawler({
     callback : function (error, result, $) {
         // $ is Cheerio by default
         //a lean implementation of core jQuery designed specifically for the server
-
-        $('a').each(function(index, a) {
-            var toQueueUrl = $(a).attr('href');
-            if (toQueueUrl.indexOf('http') === 0 && toQueueUrl.indexOf('dmm.co.jp') > -1) {
-                console.log('crawl url: ' + toQueueUrl);
-                crawler_dmm_queue(toQueueUrl);
-            }
-        });
+        crawler_ParseElement($);
     }
 });
-
-function crawler_dmm() {
-    c.queue('http://www.dmm.co.jp');
-}
 
 function crawler_dmm_queue(url) {
     c.queue([{
@@ -30,28 +22,72 @@ function crawler_dmm_queue(url) {
 
         callback: function (error, result, $) {
             if($) {
-                $('a').each(function(index, a) {
-                    var toQueueUrl = $(a).attr('href');
-                    if (toQueueUrl && toQueueUrl.indexOf('http') === 0 && toQueueUrl.indexOf('dmm.co.jp') > -1 && crawler_parseUrl(toQueueUrl)) {
-                        console.log('crawl url: ' + toQueueUrl);
-                        crawler_dmm_queue(toQueueUrl);
-                    }
-                });
+                crawler_ParseElement($);
             }
         }
     }]);
 }
 
-function crawler_parseUrl(url) {
-    var returnValue = url_array.indexOf(url) > -1;
-    returnValue = !returnValue;
-    if (!returnValue) {
-    } else {
-        url_array.push(url);
-        console.log("crawl url length: " + url_array.length);
-    }
+function crawler_ParseElement($) {
+    if($) {
+        $('a').each(function(index, a) {
+            var toQueueUrl = $(a).attr('href');
+            if (toQueueUrl && toQueueUrl.indexOf('http') === 0 && toQueueUrl.indexOf('dmm.co.jp') > -1 && !crawler_CheckUrlExist(toQueueUrl)) {
+                // console.log('crawl url: ' + toQueueUrl);
+                crawler_dmm.ParseUrl_dmm(toQueueUrl);
+                crawler_dmm_queue(toQueueUrl);
+            }
+        });
 
-    return returnValue;
+        // $('img').each(function(index, img) {
+        //     var toQueueUrl = $(img).attr('src');
+        //     if (toQueueUrl && toQueueUrl.indexOf('http') === 0 && toQueueUrl.indexOf('dmm.co.jp') > -1 && !crawler_CheckUrlExist(toQueueUrl)) {
+        //         console.log('crawl url: ' + toQueueUrl);
+        //         crawler_dmm_queue(toQueueUrl);
+        //     }
+        // });
+    }
 }
 
-crawler_dmm();
+function crawler_CheckUrlExist(url) {
+    var isUrlExist = url_array.indexOf(url) > -1;
+    if (!isUrlExist) {
+        url_array.push(url);
+        // console.log("crawl url length: " + url_array.length);
+    }
+
+    return isUrlExist;
+}
+
+
+
+function crawler_CheckUrlInBlackList(url) {
+}
+
+var crawler_dmm = {
+    url_array_video : [],
+    Queue : function() {
+        c.queue('http://www.dmm.co.jp');
+    },
+    ParseUrl_dmm : function(url) {
+        if (url.indexOf('digital') > -1 && url.indexOf('videoa') > -1 && url.indexOf('detail') > -1) {
+            // console.log(url);
+            crawler_dmm.CheckUrlExist_dmm_Video(url);
+        }
+    },
+    ParseImg_dmm : function(url) {
+
+    },
+    CheckUrlExist_dmm_Video : function(url) {
+        var isUrlExist = crawler_dmm.url_array_video.indexOf(url) > -1;
+        if (!isUrlExist) {
+            crawler_dmm.url_array_video.push(url);
+            // console.log("crawl video length: " + crawler_dmm.url_array_video.length);
+            console.log("crawl video: " + url);
+        }
+
+        return isUrlExist;
+    }
+};
+
+crawler_dmm.Queue();
